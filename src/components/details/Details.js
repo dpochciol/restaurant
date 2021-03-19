098af2abe3458d5003f8e6a2cb5588e83104b5d8
Dodo30 <d.pochciol@gmail.com>
2021-03-19 13:39:43 +0100

    frontend

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import Map from '../map/Map';
 
-const Details = ({ select }) => {
+const Details = ({select}) => {
   console.log(select);
   return (
-    <div>
-      {select.name}<br />
-      {select.city.name}<br />
-      {select.description}<br />
+
+    <div className="container">
+      <div className="row" style={{marginTop:100}}>
+        <div className="col-sm">
+
+          <div className="card p-2" style={{width: 600}}>
+          <img src={select.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{select.name}</h5>
+              <p className="card-text">{select.city.name}</p>
+              <p className="card-text">{select.description}</p>
+            </div>
+          </div>
+
+
+        </div>
+        <div className="col-sm">
+          <Map values={[select]} />
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import './map.css';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import markerCustom from "../../img/icon.png";
+import "leaflet/dist/images/marker-shadow.png";
+import { Icon } from "leaflet";
+
+const Map = ({values}) => {
+  console.log(values)
+  const icon = new Icon({
+    iconUrl: markerCustom,
+    iconSize: [35, 35],
+  });
 
 
-const Map = () => {
-  return(
-    <div id="mapid"></div>
-  )
-}
+  const renderMarker = values.map((location, id) => {
+    
+    return(
+      <Marker key={id}
+        position={[
+          location.location.coordinates[0],
+          location.location.coordinates[1],
+        ]}
+        icon={icon}
+      >
+        <Popup>
+        </Popup>
+      </Marker>
+    )
+  })
+
+
+  return (
+    <div class="card p-2 m-2" style={{ width: "600px" }}>
+      <MapContainer
+      style={{height: 600}}
+      center={[values[0].location.coordinates[0], values[0].location.coordinates[1]]}
+      zoom={11}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {renderMarker}
+
+    </MapContainer>
+  </div>
+
+  );
+};
 
 export default Map;
